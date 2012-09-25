b0aafb8b48b04e31f15e2d89a8ea6407065a4d74
rossmarc@gmail.com <rossmarc@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-09-25 15:14:03 +0000

    add class : TestNewClass

---
+++ trunk/src/chessP2P/ihmlogin/TestNewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chessP2P.ihmlogin;
+
+/**
+ *
+ * @author marcrossi
+ */
+public class TestNewClass {
+    
+}
