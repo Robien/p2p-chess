161d6f3beb2887cf9108f683c3b5601f30291595
caroline.zawada <caroline.zawada@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-09-25 12:53:16 +0000

    **test**

---
+++ trunk/ChessP2P/src/lo23/connexion/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lo23.connexion;
+
+/**
+ *
+ * @author zawadaca
+ */
+public class NewClass {
+// test
+}
