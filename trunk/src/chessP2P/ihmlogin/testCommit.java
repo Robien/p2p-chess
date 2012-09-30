4b7aea9c5e2752004ae00f6e963a457e6ef75dcc
louispontoise@gmail.com <louispontoise@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-09-30 17:32:38 +0000

    testing commit

---
+++ trunk/src/chessP2P/ihmlogin/testCommit.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chessP2P.ihmlogin;
+
+/**
+ *
+ * @author louis
+ */
+public class testCommit {
+    
+}
