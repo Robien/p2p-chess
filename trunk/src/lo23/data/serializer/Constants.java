5689739d56dc2fc1d4983a32d7418a4b501b58d2
stephan.donin@gmail.com <stephan.donin@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-10-09 13:49:11 +0000

    Data serialization: Constants add

---
+++ trunk/src/lo23/data/serializer/Constants.java
@@ -0,0 +1,11 @@
+package lo23.data.serializer;
+
+/**
+ * This class contains every path where objects will be serialized and saved
+ */
+public class Constants
+{
+	// Paths where will be saved serialized objects
+	static public final String PROFILES_PATH = "data/games/";
+	static public final String GAMES_PATH = "data/profiles";
+}
