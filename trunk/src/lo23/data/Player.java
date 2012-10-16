93512d0be1944fc00c7954cbb52799563a0ff8bf
khamidou@gmail.com <khamidou@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-10-16 13:53:41 +0000

    implementé méthodes.

--- trunk/src/lo23/data/Player.java
+++ trunk/src/lo23/data/Player.java
@@ -10,5 +10,28 @@ package lo23.data;
  * @author khamidou
  */
 public class Player {
+    public enum COLOR {BLACK, WHITE};
+    private COLOR playerColor;
+    private long time = 0;
+    private long startTime = 0;
+    private long endTime = 0;
+    private long remainingTime = 0;
 
+    public Player(COLOR playerColor, long remainingTime) {
+        this.playerColor = playerColor;
+        this.remainingTime = remainingTime;
+    }
+   
+    public void startTime() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stopTime() {
+        endTime = System.currentTimeMillis();
+        time += endTime - startTime;
+    }
+
+    public long getRemainingTime() {
+        return remainingTime - time;
+    }
 }
