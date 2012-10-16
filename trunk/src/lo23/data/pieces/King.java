6753ede11a61969c8bf76206fd82774d7259b943
khamidou@gmail.com <khamidou@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-10-16 14:19:44 +0000

    oublié de commiter des fichiers

--- trunk/src/lo23/data/pieces/King.java
+++ trunk/src/lo23/data/pieces/King.java
@@ -1,11 +1,18 @@
 package lo23.data.pieces;
 
+import java.util.List;
 import lo23.data.GamePiece;
+import lo23.data.Position;
 
 /**
  *
  * @author khamidou
  */
 public class King extends GamePiece {
+
+    @Override
+    public List<Position> getPossibleMoves() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
