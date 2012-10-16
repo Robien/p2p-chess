4a7a550b73ba9d96425a1d1bd5ce5010724b0567
khamidou@gmail.com <khamidou@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-10-16 13:03:17 +0000

    Ajoute la classe Move

---
+++ trunk/src/lo23/data/Move.java
@@ -0,0 +1,31 @@
+package lo23.data;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author khamidou
+ */
+public class Move extends Event implements Serializable {
+    Position from;
+    Position to;
+    GamePiece piece;
+
+    public Move(Position from, Position to, GamePiece piece) {
+        this.from = from;
+        this.to = to;
+        this.piece = piece;
+    }
+
+    public Position getFrom() {
+        return from;
+    }
+
+    public GamePiece getPiece() {
+        return piece;
+    }
+
+    public Position getTo() {
+        return to;
+    }
+}
