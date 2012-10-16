94a8daa0c12b1e7fb252ddcf553550bac99293e8
stephan.donin@gmail.com <stephan.donin@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-10-16 12:18:56 +0000

    Begin of the Game class

---
+++ src/lo23/data/Game.java
@@ -0,0 +1,51 @@
+package lo23.data;
+
+import java.util.Date;
+
+
+
+public class Game
+{
+    private String gameId;
+    private Date start;
+    private Date end;
+    private float duration;     // Time in seconds
+    
+
+    /**
+     * Constructor
+     * 
+     * @param id The identifier which will be associated to the created object
+     */
+    public Game(String id)
+    {
+        gameId = id;
+        start = new Date();
+        end = null;
+        duration = 0;
+    }
+    
+    
+    /**
+     * Getter for the gameId attribute
+     * 
+     * @return The gameId attribute
+     */
+    public String getGameId()
+    {
+        return gameId;
+    }
+    
+    
+    public void start()
+    {
+    }
+    
+    public void stop()
+    {
+    }
+    
+    public void resume()
+    {
+    }
+}
