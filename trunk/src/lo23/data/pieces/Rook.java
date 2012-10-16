9526dc429ca8c950c62fafd7382a076214166911
khamidou@gmail.com <khamidou@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-10-16 13:30:16 +0000

    Redefini des méthodes en abstract

--- trunk/src/lo23/data/pieces/Rook.java
+++ trunk/src/lo23/data/pieces/Rook.java
@@ -5,12 +5,19 @@
 
 package lo23.data.pieces;
 
+import java.util.List;
 import lo23.data.GamePiece;
+import lo23.data.Position;
 
 /**
  *
  * @author khamidou
  */
 public class Rook extends GamePiece {
+
+    @Override
+    public List<Position> getPossibleMoves() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
