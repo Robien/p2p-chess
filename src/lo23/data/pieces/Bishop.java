a117f2cab6fe15f5fa98332d4244c22f4d057cdb
r.guyard <r.guyard@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-10-23 14:11:45 +0000

    Bishop implementation methode getPossibleMoves

--- src/lo23/data/pieces/Bishop.java
+++ src/lo23/data/pieces/Bishop.java
@@ -1,9 +1,11 @@
 package lo23.data.pieces;
 
+import java.util.ArrayList;
 import java.util.List;
 import lo23.data.Game;
 import lo23.data.Player;
 import lo23.data.Position;
+
 
 /**
  *
@@ -24,7 +26,82 @@ public class Bishop extends GamePiece {
     
     @Override
     public List<Position> getPossibleMoves() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    //    throw new UnsupportedOperationException("Not supported yet.");
+
+
+        ArrayList<Position> positions = new ArrayList<Position>();
+        boolean xpyp, xmyp, xpym, xmpm;
+        xpyp = true;
+        xmyp = true;
+        xpym = true;
+        xmpm = true;
+
+        int x = getPosition().getX();
+        int y = getPosition().getY();
+
+        Game game = getGame();
+
+        for (int i = 0; i < 8 && (xpyp || xmyp || xpym ||xmpm);i++)
+        {
+
+
+            if (xpyp && x+i < 8 && y+i < 8 && game.getPieceAtXY(x+i, y+i) == null)
+            {
+                positions.add(new Position(x+i, y+i));
+            }
+            else
+            {
+                if (game.getPieceAtXY(x+i, y+i) != null && game.getPieceAtXY(x+i, y+i).getOwner().getColor() != getOwner().getColor())
+                {
+                    positions.add(new Position(x+i, y+i));
+                }
+                xpyp = false;
+            }
+
+            if (xmyp && x-i >= 0 && y+i < 8 && game.getPieceAtXY(x-i, y+i) == null)
+            {
+                positions.add(new Position(x-i, y+i));
+            }
+            else
+            {
+                if (game.getPieceAtXY(x-i, y+i) != null && game.getPieceAtXY(x-i, y+i).getOwner().getColor() != getOwner().getColor())
+                {
+                    positions.add(new Position(x-i, y+i));
+                }
+                xmyp = false;
+            }
+
+            if (xpym && x+i < 8 && y-i >= 0 && game.getPieceAtXY(x+i, y-i) == null)
+            {
+                positions.add(new Position(x+i, y-i));
+            }
+            else
+            {
+                if (game.getPieceAtXY(x+i, y-i) != null && game.getPieceAtXY(x+i, y-i).getOwner().getColor() != getOwner().getColor())
+                {
+                positions.add(new Position(x+i, y-i));
+                }
+                xpym = false;
+            }
+
+            if (xmpm && x-i >= 0 && y-i >= 8 && game.getPieceAtXY(x+i, y+i) == null)
+            {
+                positions.add(new Position(x-i, y-i));
+            }
+            else
+            {
+                   if (game.getPieceAtXY(x-i, y-i) != null && game.getPieceAtXY(x-i, y-i).getOwner().getColor() != getOwner().getColor())
+                {
+                positions.add(new Position(x-i, y-i));
+                }
+                xmpm = false;
+            }
+
+        }
+
+
+        
+        return positions;
     }
 
 }
