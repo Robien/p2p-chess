035c8f46c124ace98f4728a54409e74d874830db
noe.gaumont <noe.gaumont@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-10-23 12:43:54 +0000

    Création d'ApplicationModel

---
+++ trunk/src/lo23/data/ApplicationModel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lo23.data;
+
+import lo23.data.managers.GameManagerInterface;
+
+/**
+ *
+ * @author Noé Gaumont
+ */
+public class ApplicationModel {
+    
+    private GameManagerInterface  gManager;
+    private ProfileManagerInterface pManager;
+
+    
+    ApplicationModel(GameManagerInterface gManager,ProfileManagerInterface pManager){
+        this.gManager=gManager;
+        this.pManager=pManager;
+    }
+    
+}
