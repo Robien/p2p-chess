dbdb57b5d5ffb6959b6d47c9938e4caa99681ee0
morgan.antonelli@gmail.com <morgan.antonelli@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-10-23 14:35:50 +0000

    timer fonctionnel

--- trunk/src/lo23/ui/grid/TimerPanel.java
+++ trunk/src/lo23/ui/grid/TimerPanel.java
@@ -5,10 +5,13 @@
 
 package lo23.ui.grid;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.border.Border;
@@ -18,14 +21,31 @@ import javax.swing.border.LineBorder;
  *
  * @author mantonel
  */
-public class TimerPanel extends JLabel {
+public class TimerPanel extends JPanel {
 
     private PlayerTimer playerTimer;
     private Border border;
+    JLabel timer;
+    JButton start;
+    JButton stop;
+    JButton pause;
+    JButton recovery;
 
     public TimerPanel() {
+        setLayout(new BorderLayout());
         setPreferredSize(new Dimension(100,100));
-        setHorizontalTextPosition(SwingConstants.CENTER);
+        //setHorizontalTextPosition(SwingConstants.CENTER);
+
+        timer = new JLabel();
+        start = new JButton("Start");
+        stop = new JButton("Stop");
+        pause = new JButton("Pause");
+        recovery = new JButton("Recovery");
+        add(timer, BorderLayout.NORTH);
+        add(start, BorderLayout.WEST);
+        add(stop, BorderLayout.EAST);
+        add(pause, BorderLayout.CENTER);
+        add(recovery, BorderLayout.SOUTH);
 
         border = new LineBorder(new Color(255));
         this.setBorder(border);
@@ -33,6 +53,34 @@ public class TimerPanel extends JLabel {
         playerTimer = new PlayerTimer(this);
         playerTimer.startTimer();
 
+        start.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                playerTimer.startTimer();
+            }
+        });
+
+        stop.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                playerTimer.stopTimer();
+            }
+        });
+
+        pause.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                playerTimer.pauseTimer();
+            }
+        });
+
+        recovery.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                playerTimer.recoveryTimer();
+            }
+        });
+
+    }
+
+    public JLabel getLabel(){
+        return timer;
     }
 
 }
