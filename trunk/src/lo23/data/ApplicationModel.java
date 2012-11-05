5ffdcd13517855ad163ace84b5678ab36ef5c07a
noe.gaumont <noe.gaumont@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-05 14:20:29 +0000

    Maj d'ApplicationModel

--- trunk/src/lo23/data/ApplicationModel.java
+++ trunk/src/lo23/data/ApplicationModel.java
@@ -4,6 +4,7 @@
  */
 package lo23.data;
 
+import lo23.communication.ISender;
 import lo23.data.managers.GameManagerInterface;
 import lo23.data.managers.ProfileManagerInterface;
 
@@ -12,14 +13,26 @@ import lo23.data.managers.ProfileManagerInterface;
  * @author Noé Gaumont
  */
 public class ApplicationModel {
-    
-    private GameManagerInterface  gManager;
-    private ProfileManagerInterface pManager;
 
-    
-    ApplicationModel(GameManagerInterface gManager,ProfileManagerInterface pManager){
-        this.gManager=gManager;
-        this.pManager=pManager;
+    private GameManagerInterface gManager;
+    private ProfileManagerInterface pManager;
+    private ISender comManager;
+
+    ApplicationModel(GameManagerInterface gManager, ProfileManagerInterface pManager, ISender comManager) {
+        this.gManager = gManager;
+        this.pManager = pManager;
+        this.comManager = comManager;
     }
-    
+
+    GameManagerInterface getGManager() {
+        return gManager;
+    }
+
+    ProfileManagerInterface getPManager() {
+        return pManager;
+    }
+
+    ISender getISender() {
+        return comManager;
+    }
 }
