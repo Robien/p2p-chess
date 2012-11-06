87f30af989aaef4fa16ccecffe603e04bce6c86c
benjamin.poirier.ent@gmail.com <benjamin.poirier.ent@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-06 13:58:30 +0000

    update Messages classes v2.1

--- trunk/src/lo23/communication/message/ConnectionMessage.java
+++ trunk/src/lo23/communication/message/ConnectionMessage.java
@@ -3,11 +3,6 @@ package lo23.communication.message;
 /**
  * Abstract class used for messages dedicated to client general communication (Multicast, Invit...).
  */
-public class ConnectionMessage extends Message {
-
-    /**
-     * Contructor of the ConnectionMessage.
-     */
-    public ConnectionMessage() {
-    }
+public abstract class ConnectionMessage extends Message {
+    
 }
