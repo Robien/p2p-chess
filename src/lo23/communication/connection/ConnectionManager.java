4a810384f642e082423f71d95bdfb8b2bc67e5d4
berteloot.luc@gmail.com <berteloot.luc@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-06 13:28:31 +0000

    Modify ConnectionManager.java

--- src/lo23/communication/connection/ConnectionManager.java
+++ src/lo23/communication/connection/ConnectionManager.java
@@ -1,12 +1,16 @@
 package lo23.communication.connection;
 
 import java.io.IOException;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import lo23.communication.ComManager;
+import lo23.communication.handle.HandleReceiveMessage;
+import lo23.communication.handle.HandleSendMessage;
 
 /**
  * This class manage the socket connections.
@@ -14,8 +18,15 @@ import lo23.communication.ComManager;
 public class ConnectionManager {
 
     private ComManager comManager;
+    
     private MulticastSocket multicastSocket;
+    private DatagramSocket datagramSocket;
+    
     private ServerSocket serverSocket;
+    private HandleSendMessage serverSendMessage;
+    private HandleReceiveMessage serverReceiveMessage;
+    
+    private Socket clientSocket;
     
     public ConnectionManager(ComManager comManager) {
         this.comManager = comManager;
@@ -23,9 +34,11 @@ public class ConnectionManager {
         try {
             multicastSocket = new MulticastSocket(ConnectionParams.multicastPort);
             multicastSocket.joinGroup(InetAddress.getByName(ConnectionParams.multicastAddress));
-            // TODO ajouter les handles puis continuer ici
+            
+            serverSocket = new ServerSocket(ConnectionParams.unicastPort); 
+            
         } catch (IOException ex) {
-            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ConnectionManager.class.getName()).log(Level.SEVERE, "Error for the initialisation of the server sockets", ex);
         }
     }
     
