f4602716173674eb6f821a279d393a4f4cbc0bb9
karim.chebbour@gmail.com <karim.chebbour@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-06 14:21:30 +0000

    Board added to GamePanel

--- trunk/src/lo23/ui/grid/GamePanel.java
+++ trunk/src/lo23/ui/grid/GamePanel.java
@@ -9,11 +9,21 @@ package lo23.ui.grid;
  * @author Karim
  */
 
-import java.awt.BorderLayout;
-import java.awt.Color;
+
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.io.File;
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
+    
+    
     
     public GamePanel() {
      super();
@@ -21,8 +31,40 @@ public class GamePanel extends JPanel {
     }
     
     private void build() {
-        setSize(680, 500); //On donne une taille à notre fenêtre
-        setBackground(Color.BLACK);
+       GridBagLayout gameBoard = new GridBagLayout();
+       setSize(680, 500); //On donne une taille à notre fenêtre
+       setLayout(gameBoard);
+       applyComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+       
+       GridBagConstraints constraints = new GridBagConstraints();
+       constraints.fill = GridBagConstraints.CENTER;
+       constraints.anchor = GridBagConstraints.CENTER;
+       
+       
+       for(int i=0; i<8; i++){
+           for(int j=0; j<8; j++){
+               constraints.gridx = i;
+               constraints.gridy = j;
+               
+               
+                    
+               if((i+j)%2 != 0){
+                    String path = getClass().getClassLoader().getResource(".").getPath();
+                    ImageIcon image = new ImageIcon(path + "lo23/ui/resources/caseB.JPG");
+                    JLabel label = new JLabel("", image, JLabel.CENTER);
+                    add(label, constraints);
+               } else {
+                    String path = getClass().getClassLoader().getResource(".").getPath();
+                    ImageIcon image = new ImageIcon(path + "lo23/ui/resources/caseN.JPG");
+                    JLabel label = new JLabel("", image, JLabel.CENTER);
+                    add(label, constraints);
+               }
+              
+              
+          
+            }    
+
+         }
        
     }
         
