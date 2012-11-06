f9baa3762275d16ace8b9b6172541bcf3ba3a30a
penotvin@gmail.com <penotvin@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-06 13:50:16 +0000

    GameMessage v0.3

--- src/lo23/communication/message/GameMessage.java
+++ src/lo23/communication/message/GameMessage.java
@@ -3,11 +3,7 @@ package lo23.communication.message;
 /**
  * Abstract class used for messages dedicated to client communication while playing (Movement, Chat...).
  */
-public class GameMessage {
+public abstract class GameMessage extends Message {
 
-    /**
-     * Contructor of the ConnectionMessage.
-     */
-    public GameMessage() {
-    }
+   
 }
