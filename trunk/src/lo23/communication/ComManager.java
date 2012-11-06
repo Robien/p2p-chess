af7859302f0d751e7373937fd4d2c10cb872dac8
penotvin@gmail.com <penotvin@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-06 14:53:58 +0000

    GameMessage v0.4

--- trunk/src/lo23/communication/ComManager.java
+++ trunk/src/lo23/communication/ComManager.java
@@ -1,16 +1,22 @@
 package lo23.communication;
 
+import java.awt.Image;
 import lo23.communication.connection.ConnectionManager;
+import lo23.data.ApplicationModel;
 import lo23.data.Constant;
 import lo23.data.Invitation;
 import lo23.data.Message;
 import lo23.data.Move;
 import lo23.data.PublicProfile;
+import lo23.data.managers.GameManager;
+import lo23.data.managers.Manager;
+import lo23.ui.login.mockManager.*;
+import lo23.utils.Enums.STATUS;
 
 /**
  * This is the main class of the communication module.
  */
-public class ComManager implements ISender {
+public class ComManager extends Manager implements ISender {
 
     // TODO faire les classes Messages
     // TODO finir les Handle
@@ -24,10 +30,20 @@ public class ComManager implements ISender {
      * @param profile the profile of the current user
      * @param receiveListener the listener who will be notified
      */
-    public void ComManager(PublicProfile profile, IReceiveListener receiveListener) {
+    public ComManager(PublicProfile profile, ApplicationModel applicationModel) {
+        super(applicationModel);
         this.currentUserProfile = profile;
-        this.receiveListener = receiveListener;
         this.connectionManager = new ConnectionManager(this);
+    }
+
+    public static void main(String args[]){
+
+        //Instantiate DataManager
+        PublicProfile profile = new PublicProfile("1", "penotvin", STATUS.INGAME, "172.22.2.3", null,"PENOT","Vincent",22);
+        ApplicationModel appModel = new ApplicationModel();
+        appModel.setComManager(new ComManager(profile,appModel));
+        appModel.setGameManager(new GameManagerMock((appModel)));
+        appModel.setProfileManager(new ProfileManagerMock(appModel));
     }
     
     public PublicProfile getCurrentUserProfile() {
