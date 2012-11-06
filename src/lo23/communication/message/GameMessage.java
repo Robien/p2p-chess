029f33d8bce847d9bc78da38ee6bb4052cca9800
benjamin.poirier.ent@gmail.com <benjamin.poirier.ent@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-06 13:33:05 +0000

    update Messages classes

--- src/lo23/communication/message/GameMessage.java
+++ src/lo23/communication/message/GameMessage.java
@@ -1,7 +1,13 @@
 package lo23.communication.message;
 
+/**
+ * Abstract class used for messages dedicated to client communication while playing (Movement, Chat...).
+ */
 public class GameMessage {
 
+    /**
+     * Contructor of the ConnectionMessage.
+     */
     public GameMessage() {
     }
 }
