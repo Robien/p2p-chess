d4e2c5335edeea17a1cb38483749251e94f573cd
loic.picavet <loic.picavet@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-06 14:59:10 +0000

    Including several managers which implements interface + ChatPanel action key with enter

--- trunk/src/lo23/ui/grid/Main.java
+++ trunk/src/lo23/ui/grid/Main.java
@@ -5,16 +5,29 @@
 package lo23.ui.grid;
 
 import javax.swing.SwingUtilities;
+import lo23.data.ApplicationModel;
 
 /**
  *
  * @author Karim
  */
 public class Main {
+        static ApplicationModel myModel;
+
+
+     private static ApplicationModel getModel(){
+         return myModel;
+     }
+
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable(){
             public void run() {
-                MainWindow fenetre = new MainWindow();
+                myModel = new ApplicationModel();
+                myModel.setGameManager(new TMP_GameManager(myModel));
+                myModel.setProfileManager(new TMP_ProfileManager(myModel));
+
+                MainWindow fenetre = new MainWindow(getModel());
                 fenetre.setVisible(true);
             }
         });
