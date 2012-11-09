e1d4dced309f8091963a7b263bda9f3afeef9970
morgan.antonelli@gmail.com <morgan.antonelli@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-09 18:32:49 +0000

    selection des cases

---
+++ trunk/src/lo23/ui/grid/GridConstants.java
@@ -0,0 +1,10 @@
+package lo23.ui.grid;
+
+public class GridConstants {
+
+	//Constants for the grid
+	public static int SQUARE_SIZE = 64;
+	public static int WINDOW_HEIGHT = 750;
+	public static int WINDOW_WIDTH = 1020;
+	
+}
--- trunk/src/lo23/ui/grid/MainWindow.java
+++ trunk/src/lo23/ui/grid/MainWindow.java
@@ -14,8 +14,13 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
 import lo23.data.ApplicationModel;
 
 public class MainWindow extends JFrame implements ActionListener {
@@ -30,17 +35,18 @@ public class MainWindow extends JFrame implements ActionListener {
 
     private void build() {
         setTitle("Chess P2P"); //On donne un titre à l'application
-        setSize(1020, 750); //On donne une taille à notre fenêtre
+        setSize(GridConstants.WINDOW_WIDTH, GridConstants.WINDOW_HEIGHT); //On donne une taille à notre fenêtre
         setLocationRelativeTo(null); //On centre la fenêtre sur l'écran
         setResizable(false); //On interdit la redimensionnement de la fenêtre
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //On dit à l'application de se fermer lors du clic sur la croix
 
         setContentPane(buildContentPanel());
+        
     }
-
+    
     private JPanel buildContentPanel() {
 
-        //Panel global
+        //Global Panel
         JPanel panel = new JPanel();
         GridBagLayout gridBagLayout = new GridBagLayout();
         panel.setLayout(gridBagLayout);
@@ -49,33 +55,52 @@ public class MainWindow extends JFrame implements ActionListener {
 
         constraints.fill = GridBagConstraints.HORIZONTAL;
 
-        //Le panneau est représenté par un tableau de 6*8 cases
+        //The panel is a grid of 6*8 squares
 
-
-        // Grille
-        GamePanel gamePanel = new GamePanel();
+        //Grid
+        final GamePanel gamePanel = new GamePanel();
         constraints.insets = new Insets(0,0,0,0);
-	constraints.gridwidth = 4;
-	constraints.gridheight = 4;
-	constraints.gridx = 1;
-	constraints.gridy = 1;
-
+		constraints.gridwidth = 4;
+		constraints.gridheight = 4;
+		constraints.gridx = 1;
+		constraints.gridy = 1;
         panel.add(gamePanel, constraints);
+        
+        //listener 
+        gamePanel.addMouseListener(new MouseAdapter() {
+    	    public void mousePressed(MouseEvent mouseEvent) {
+    	        if ( SwingUtilities.isLeftMouseButton(mouseEvent) ) {
+    	        	//left button, main button
+    	        	int x = mouseEvent.getX();
+    	        	int y = mouseEvent.getY();
+    	        	int xSquare = (x - x%GridConstants.SQUARE_SIZE)/GridConstants.SQUARE_SIZE;
+    	        	int ySquare = (y - y%GridConstants.SQUARE_SIZE)/GridConstants.SQUARE_SIZE;
+    	        	gamePanel.addTexture(xSquare, ySquare);
+    	        	
+    	        } else if(SwingUtilities.isMiddleMouseButton(mouseEvent) ) {
+    	        	//middle button
+    	        	
+    	        } else if(SwingUtilities.isRightMouseButton(mouseEvent)) {
+    	        	//right button
+    	        	
+    	        } 
+    	    }
+        });
 
-        //Test Ajout Timer, à remplacer par profilPanel
+        //Test Ajout Timer, a� remplacer par profilPanel
         constraints.gridwidth = 6;
-	constraints.gridheight = 1;
-	constraints.gridx = 1;
-	constraints.gridy = 0;
+		constraints.gridheight = 1;
+		constraints.gridx = 1;
+		constraints.gridy = 0;
 
         TimerPanel timerPanel = new TimerPanel();
         panel.add(timerPanel, constraints);
 
         //Test Ajout Timer
         constraints.gridwidth = 6;
-	constraints.gridheight = 1;
-	constraints.gridx = 1;
-	constraints.gridy = 5;
+		constraints.gridheight = 1;
+		constraints.gridx = 1;
+		constraints.gridy = 5;
 
         TimerPanel timerPanel2 = new TimerPanel();
         panel.add(timerPanel2, constraints);
@@ -83,9 +108,9 @@ public class MainWindow extends JFrame implements ActionListener {
         //Chat panel
         constraints.insets = new Insets(50,50,0,0);
         constraints.gridwidth = 2;
-	constraints.gridheight = 6;
-	constraints.gridx = 6;
-	constraints.gridy = 0;
+		constraints.gridheight = 6;
+		constraints.gridx = 6;
+		constraints.gridy = 0;
 
         ChatPanel2 chatPanel = new ChatPanel2(myModel);
         panel.add(chatPanel, constraints);
@@ -93,16 +118,6 @@ public class MainWindow extends JFrame implements ActionListener {
         return panel;
     }
 
-//    private GridBagConstraints modifyConstraint(int width, float weightx, float weighty, int height) {
-//        GridBagConstraints c = new GridBagConstraints();
-//        c.gridwidth = width;
-//        c.weightx = weightx;
-//        c.weighty = weighty;
-//        c.gridheight = height;
-//        c.fill = GridBagConstraints.HORIZONTAL;
-//
-//        return c;
-//    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
