5a8defe547573071f79f44d4fdaacd775356f8ad
morgan.antonelli@gmail.com <morgan.antonelli@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-11 01:06:58 +0000

    position d'un pion

---
+++ trunk/src/lo23/ui/grid/PositionOnBoard.java
@@ -0,0 +1,33 @@
+package lo23.ui.grid;
+
+public class PositionOnBoard {
+
+	private int x;
+	private int y;
+	
+	public PositionOnBoard(int xPosition, int yPosition) {
+		x = xPosition;
+		y = yPosition;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	@Override
+	public boolean equals(Object P) {
+		if (((PositionOnBoard) P).getX() == x && ((PositionOnBoard) P).getY() == y) return true;
+		else return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		String temp = String.valueOf(x) + String.valueOf(y);
+		return Integer.valueOf(temp);
+	}
+	
+}
