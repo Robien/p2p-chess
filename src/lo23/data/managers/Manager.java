ba8e6194cfdca223ea10d1c828dd7d37d5f42bd1
stephan.donin@gmail.com <stephan.donin@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-13 14:54:05 +0000

    The Manager class now encapsulates a Publish / Subscribe mechanism for the communication between every module.

--- src/lo23/data/managers/Manager.java
+++ src/lo23/data/managers/Manager.java
@@ -1,5 +1,7 @@
 package lo23.data.managers;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import lo23.data.ApplicationModel;
 
 /**
@@ -9,16 +11,45 @@ import lo23.data.ApplicationModel;
  */
 public class Manager {
 
+
+    private PropertyChangeSupport notifier;
+
+
     private ApplicationModel app;
 
 
     public Manager(ApplicationModel app) {
         this.app = app;
+        notifier = new PropertyChangeSupport(this);
     }
 
 
     public ApplicationModel getApplicationModel() {
         return app;
+    }
+
+
+    /**
+     * This method implements the Publish / Subscribe pattern.
+     * It notifies this that the listener argument wants to received data sent on the channel given as argument
+     *
+     * @param listener The object that wants to received notification for the specific channel
+     * @param channel The channel onto the listener argument will be listening to
+     */
+    public void subscribe(PropertyChangeListener listener, String channel) {
+        notifier.addPropertyChangeListener(channel, listener);
+    }
+
+    /**
+     * This method implements the Publish / Subscribe pattern.
+     * It sends data to registered subscribers, on a specific channel.
+     *
+     * @param channel The channel onto data are sent.
+     * @param value The data to be sent.
+     */
+    public void publish(String channel, Object value)
+    {
+        notifier.firePropertyChange(channel, null, value);
     }
 
 
