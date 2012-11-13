f3c3f8f9a53613ac309b6c84a261bfe5cd008e5e
khamidou <khamidou@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-13 13:40:27 +0000

    stub code to make stuff work

--- src/lo23/data/serializer/tests/TestInit.java
+++ src/lo23/data/serializer/tests/TestInit.java
@@ -7,10 +7,15 @@ package lo23.data.serializer.tests;
 
 import lo23.data.ApplicationModel;
 import lo23.data.Game;
+import lo23.data.Invitation;
+import lo23.data.NewInvitation;
 import lo23.data.Player;
+import lo23.data.Profile;
+import lo23.data.PublicProfile;
 import lo23.data.managers.GameManager;
 import lo23.data.managers.ProfileManager;
 import lo23.utils.Enums.COLOR;
+import lo23.utils.Enums.STATUS;
 
 /**
  *
@@ -20,11 +25,18 @@ import lo23.utils.Enums.COLOR;
 public class TestInit {
     public static void main(String[] args) {
         ApplicationModel app = new ApplicationModel();
+
+
+
         app.setGameManager(new GameManager(app));
         app.setProfileManager(new ProfileManager(app));
         Player p1 = new Player(COLOR.WHITE, 0);
         Player p2 = new Player(COLOR.BLACK, 0);
-        Game gm = new Game();
+
+        Profile pHost = new Profile("", "host", "", STATUS.INGAME, "", null, "", "", 21);
+        Profile pGuest = new Profile("", "host", "", STATUS.INGAME, "", null, "", "", 21);
+        NewInvitation inv = new NewInvitation(pHost.getPublicProfile(), pGuest.getPublicProfile());
+        Game gm = app.getGManager().createGame(inv);
         gm.buildPieces();
         gm.dumpBoard();
     }
