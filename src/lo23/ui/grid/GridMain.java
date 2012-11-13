3164fbe6fad5ae0829a21b78e1b32d90f84ee20c
loic.picavet@gmail.com <loic.picavet@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-13 13:50:29 +0000

    Integration of Managers

--- src/lo23/ui/grid/GridMain.java
+++ src/lo23/ui/grid/GridMain.java
@@ -5,6 +5,7 @@
 package lo23.ui.grid;
 
 import javax.swing.SwingUtilities;
+import lo23.communication.ComManager;
 import lo23.data.ApplicationModel;
 
 /**
@@ -23,10 +24,23 @@ public class GridMain {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable(){
             public void run() {
+                /*
+                 * Important : All of yours interactions with managers will be develop like that :
+                 *
+                 * You have to create a constructor which takes parameters ApplicationModel
+                 *
+                 * And all your interactions will work like that :
+                 *
+                 * myModel.getManageYouWant().callFunctionYouWant()
+                 *
+                 * You can find an example in ChatPanel2 or GamePanel
+                 *
+                 */
                 myModel = new ApplicationModel();
                 myModel.setGameManager(new TMP_GameManager(myModel));
-//                myModel.setProfileManager(new TMP_ProfileManager(myModel));
+               myModel.setProfileManager(new TMP_ProfileManager(myModel));
 
+               //myModel.setComManager(new TMP_ComManager(null, myModel))
                 MainWindow fenetre = new MainWindow(getModel());
                 fenetre.setVisible(true);
             }
