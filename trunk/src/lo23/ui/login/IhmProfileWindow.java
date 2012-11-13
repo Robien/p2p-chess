2a33a56f07ce36ff239087e87ff7c09f4a9f69c8
remi.clermont@gmail.com <remi.clermont@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-13 15:10:18 +0000

    75% profileWindows

--- trunk/src/lo23/ui/login/IhmProfileWindow.java
+++ trunk/src/lo23/ui/login/IhmProfileWindow.java
@@ -4,6 +4,7 @@
  */
 package lo23.ui.login;
 
+import java.awt.event.ActionEvent;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -13,6 +14,9 @@ import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import lo23.data.PublicProfile;
+import lo23.data.Profile;
 
 /**
  *
@@ -21,20 +25,44 @@ import javax.swing.ImageIcon;
 public class IhmProfileWindow extends JFrame{
 
     private IhmLoginModel ihmLoginModel;
-    private Boolean statusValue = false;
+    
+    public static final int MODIFY = 0;
+    public static final int CREATE = 1;
+    public static final int READ = 2;
+    
+    private int status;
 
     /**
      * Creates new form IhmProfileWindow
+     * Status : Modify, create or read.
+     * PublicProfile : give null for create and modify, give distantProfile for read
      */
-    public IhmProfileWindow(IhmLoginModel ihmLoginModel) {
+    public IhmProfileWindow(IhmLoginModel ihmLoginModel,int status, PublicProfile publicProfile) {
         super();
         this.ihmLoginModel = ihmLoginModel;
+        this.status = status;
         initComponent();
     }
 
     private void initComponent() {
-        setTitle("Gestion du profil"); //On donne un titre à l'application
-        setSize(400, 450); //On donne une taille à notre fenêtre
+        switch(status){
+            case MODIFY :
+                setTitle("Gestion du profil"); //On donne un titre à l'application
+                setSize(400, 450); //On donne une taille à notre fenêtre
+                break;
+            case READ :
+                setTitle("Consultation d'un profil"); //On donne un titre à l'application
+                setSize(400, 450); //On donne une taille à notre fenêtre
+                break;
+            case CREATE :
+                setTitle("Création d'un profil"); //On donne un titre à l'application
+                setSize(400, 450); //On donne une taille à notre fenêtre
+                break;
+                
+            
+        }
+        
+        
         setLocationRelativeTo(null); //On centre la fenêtre sur l'écran
         setResizable(false); //On interdit la redimensionnement de la fenêtre
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //On dit à l'application de se fermer lors du clic sur la croix
@@ -48,81 +76,149 @@ public class IhmProfileWindow extends JFrame{
         GroupLayout layout = new GroupLayout(panel);
         panel.setLayout(layout);
 
+        //PROFILE INFORMATION PART
         JLabel login = new JLabel();
         login.setText("Login");
-
+        JTextField loginField = new JTextField();
 
         JLabel password = new JLabel();
         password.setText("Mot de passe");
+        JPasswordField jPasswordField1 = new JPasswordField();
 
         JLabel passwordConfirm = new JLabel();
         passwordConfirm.setText("Ressaisissez votre mot de passe");
+        JPasswordField jPasswordField2 = new JPasswordField();
 
         JLabel firstName = new JLabel();
         firstName.setText("Nom");
-
+        JTextField firstNameField = new JTextField();
+        
         JLabel lastName = new JLabel();
         lastName.setText("Prénom");
+        JTextField lastNameField = new JTextField();
 
         JLabel age = new JLabel();
         age.setText("Âge");
+        JTextField ageField = new JTextField();
+        
+        
+        JButton exportProfileButton = new JButton();
+        exportProfileButton.setText("Exporter le profil");
+        // Listener
+        exportProfileButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exportProfilePerformed(evt);
+            }
+        });
 
-        JTextField login_field = new JTextField("1");
-        login_field.setEditable(statusValue);
-        login_field.setVisible(statusValue);
+        JButton applyButton = new JButton();
+        // Listener
+        applyButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                applyPerformed(evt);
+            }
+        });
 
         // The separators
         JSeparator jSeparator1 = new JSeparator();
         JSeparator jSeparator2 = new JSeparator();
-
-        JTextField jTextField9 = new JTextField("2");
-        JTextField jTextField10 = new JTextField("3");
-        JTextField jTextField11 = new JTextField("4");
-
-        JButton jButton1 = new JButton();
-        jButton1.setText("Changer votre avatar");
-
+   
+        //TODO image operationnel
+        JButton changeImageButton = new JButton();
+        changeImageButton.setText("Changer votre avatar");
+        
         //JLabel lab = new JLabel(new ImageIcon("src\\votre_image.png"));
         //lab.setBounds(0, 0, 270, 230);
         ImageIcon userIcon = new ImageIcon("gaetan.jpg");
-        JLabel jLabel7 = new JLabel(userIcon);
+        JLabel profileImage = new JLabel(userIcon);
+        
+        switch(status){
+            case MODIFY :
+                //TODO define text from profile
+                loginField.setEditable(true);
+                lastNameField.setEditable(true);
+                firstNameField.setEditable(true);
+                ageField.setEditable(true);
+                
+                Profile currentProfile = ihmLoginModel.getApplicationModel().getPManager().getCurrentProfile();
+                loginField.setText(currentProfile.getPseudo());
+                lastNameField.setText(currentProfile.getName());
+                firstNameField.setText(currentProfile.getFirstName());
+                ageField.setText(String.valueOf(currentProfile.getAge()));
+                //TODO : avatar profile
+                
+                
+                break;
+            case READ :
+                //TODO define text from profile
+                loginField.setEditable(false);
+                lastNameField.setEditable(false);
+                firstNameField.setEditable(false);
+                ageField.setEditable(false);
+                password.setVisible(false);
+                passwordConfirm.setVisible(false);
+                jPasswordField1.setVisible(false);
+                jPasswordField2.setVisible(false);
+                changeImageButton.setVisible(false);
+                setSize(400, 450); //On donne une taille à notre fenêtre
+                exportProfileButton.setVisible(false);
+                applyButton.setText("Revenir à la liste des parties");
+                
+                Profile publicProfile = ihmLoginModel.getApplicationModel().getPManager().getCurrentProfile();
+                loginField.setText(publicProfile.getPseudo());
+                lastNameField.setText(publicProfile.getName());
+                firstNameField.setText(publicProfile.getFirstName());
+                ageField.setText(String.valueOf(publicProfile.getAge()));
+                
+                break;
+            case CREATE :
+                loginField.setEditable(true);
+                loginField.setEditable(true);
+                lastNameField.setEditable(true);
+                firstNameField.setEditable(true);
+                ageField.setEditable(true);
+                setSize(400, 450); //On donne une taille à notre fenêtre
+                exportProfileButton.setVisible(false);
+                applyButton.setText("Valider l'inscription");
+                break;
+        }
 
-        JPasswordField jPasswordField1 = new JPasswordField("5");
-        JPasswordField jPasswordField2 = new JPasswordField("67");
+        
+        //STATISTIC PART
+        JLabel gamesWonLabel = new JLabel();
+        gamesWonLabel.setText("Parties gagnées");
+        JLabel gamesLostLabel = new JLabel();
+        gamesLostLabel.setText("Parties perdues");
 
-        JLabel jLabel8 = new JLabel();
-        jLabel8.setText("Parties gagnées");
-        JLabel jLabel9 = new JLabel();
-        jLabel9.setText("Parties perdues");
+        JLabel gamesDrawLabel = new JLabel();
+        gamesDrawLabel.setText("Parties nulles");
 
-        JLabel jLabel10 = new JLabel();
-        jLabel10.setText("Parties nulles");
-
-        JTextField jTextField12 = new JTextField("8");
-        JTextField jTextField13 = new JTextField("9");
-        JTextField jTextField14 = new JTextField("10");
-
-        JButton jButton2 = new JButton();
-        jButton2.setText("Exporter le profil");
-
-        JButton jButton3 = new JButton();
-        jButton3.setText("Appliquer");
-
+        //TODO : from statistic profile
+        JTextField gamesWonField = new JTextField("8");
+        JTextField gamesLostField = new JTextField("9");
+        JTextField gamesDrawField = new JTextField("10");
+        
+        gamesWonField.setEditable(false);
+        gamesLostField.setEditable(false);
+        gamesDrawField.setEditable(false);
+        
 
 
+
+        //LAYOUT PART
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(36, 36, 36)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton2)
+                        .addComponent(exportProfileButton)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton3))
+                        .addComponent(applyButton))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel7)
+                        .addComponent(profileImage)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1))
+                        .addComponent(changeImageButton))
                     .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -135,23 +231,23 @@ public class IhmProfileWindow extends JFrame{
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jPasswordField1)
-                            .addComponent(login_field)
-                            .addComponent(jTextField9, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
-                            .addComponent(jTextField10, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
-                            .addComponent(jTextField11, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
+                            .addComponent(loginField)
+                            .addComponent(lastNameField, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
+                            .addComponent(firstNameField, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
+                            .addComponent(ageField, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
                             .addComponent(jPasswordField2, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel10)
+                        .addComponent(gamesDrawLabel)
                         .addGap(96, 96, 96)
-                        .addComponent(jTextField14, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE))
+                        .addComponent(gamesDrawField, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel8)
+                        .addComponent(gamesWonLabel)
                         .addGap(82, 82, 82)
-                        .addComponent(jTextField12, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE))
+                        .addComponent(gamesWonField, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel9)
+                        .addComponent(gamesLostLabel)
                         .addGap(84, 84, 84)
-                        .addComponent(jTextField13, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE))
+                        .addComponent(gamesLostField, javax.swing.GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE))
                     .addComponent(jSeparator2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 218, Short.MAX_VALUE))
                 .addGap(45, 45, 45))
         );
@@ -160,12 +256,12 @@ public class IhmProfileWindow extends JFrame{
             .addGroup(layout.createSequentialGroup()
                 .addGap(35, 35, 35)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jLabel7))
+                    .addComponent(changeImageButton)
+                    .addComponent(profileImage))
                 .addGap(37, 37, 37)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(login)
-                    .addComponent(login_field, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(loginField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(password)
@@ -177,40 +273,56 @@ public class IhmProfileWindow extends JFrame{
                 .addGap(6, 6, 6)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(firstName)
-                    .addComponent(jTextField9, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(lastNameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lastName)
-                    .addComponent(jTextField10, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(firstNameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(age)
-                    .addComponent(jTextField11, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(ageField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel8)
-                    .addComponent(jTextField12, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(gamesWonLabel)
+                    .addComponent(gamesWonField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel9)
-                    .addComponent(jTextField13, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(gamesLostLabel)
+                    .addComponent(gamesLostField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel10)
-                    .addComponent(jTextField14, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(gamesDrawLabel)
+                    .addComponent(gamesDrawField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 9, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2)
-                    .addComponent(jButton3))
+                    .addComponent(exportProfileButton)
+                    .addComponent(applyButton))
                 .addContainerGap())
         );
         return panel;
     }
-     private void jPasswordField2ActionPerformed(java.awt.event.ActionEvent evt) {
-        // TODO add your handling code here:
-    }
+     
+     //TODO actionListener pour les deux bouttons : switch
+     private void applyPerformed(java.awt.event.ActionEvent evt) {
+         switch(status){
+            case MODIFY :
+                
+                break;
+            case READ :
+                break;
+            case CREATE :
+
+                break;
+        }
+     }
+     private void exportProfilePerformed(java.awt.event.ActionEvent evt) {
+         //open explorer to select the location
+         final JFileChooser fc = new JFileChooser();
+         int returnVal = fc.showOpenDialog(this);
+     }
 }
