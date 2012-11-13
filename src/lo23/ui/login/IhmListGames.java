ad9a6971dfc88f6cd936ecf6a772dbd08b5bd615
rossmarc@gmail.com <rossmarc@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-13 19:47:56 +0000

    add classes in Utils

--- src/lo23/ui/login/IhmListGames.java
+++ src/lo23/ui/login/IhmListGames.java
@@ -11,6 +11,8 @@
 
 package lo23.ui.login;
 
+import lo23.utils.JTableButtonMouseListener;
+import lo23.utils.JTableButtonRenderer;
 import javax.swing.table.TableCellRenderer;
 import lo23.data.ApplicationModel;
 import lo23.ui.login.mockManager.CommManagerMock;
