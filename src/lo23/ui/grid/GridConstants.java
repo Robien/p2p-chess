5b74c2376dd1c10b67e0e358754b4aea53aeb957
guilhem.paoli@gmail.com <guilhem.paoli@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-14 14:43:55 +0000

    update sound

--- src/lo23/ui/grid/GridConstants.java
+++ src/lo23/ui/grid/GridConstants.java
@@ -4,7 +4,7 @@ public class GridConstants {
 
 	//Constants for the grid
 	public static int SQUARE_SIZE = 64;
-	public static int WINDOW_HEIGHT = 750;
+	public static int WINDOW_HEIGHT = 700;
 	public static int WINDOW_WIDTH = 1020;
 	
 }
