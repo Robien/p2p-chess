9a73bf5f3693d3737d184650fdb5d692a9c06a41
guilhem.paoli@gmail.com <guilhem.paoli@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-14 14:44:10 +0000

    update sound

--- src/lo23/ui/grid/Launch_Sound.java
+++ src/lo23/ui/grid/Launch_Sound.java
@@ -8,33 +8,60 @@ import java.util.logging.Logger;
  *
  * @author guigou
  */
-public class Launch_Sound implements Runnable{
+public class Launch_Sound extends Thread implements Runnable{
         Thread a;
         Sound s;
+        Noise n;
+        String sound;
         boolean enpause;
-        public Launch_Sound()
+        
+       
+        public Launch_Sound(String sound)
         {
-          a = new Thread(this);
-          s = new Sound();
-          enpause=false;
+          
+         a = new Thread(this);
+         if(sound.equals("chess.wav") || sound.equals("chess_login.wav"))
+         {
+               s = new Sound();
+         }
+         else
+         {
+               n = new Noise();
+         }
+             
+       
+         enpause=false;
+         this.sound = sound;
+         
         }
+      
 
         
+    @Override
         public void run()
         {    
-            while(true)
-            {
+            
                 String path = getClass().getClassLoader().getResource(".").getPath();
                 try 
                 {
-                    s.readAudioFile(path + "lo23/ui/resources/pendant_partie.wav");
-
+                    
+                    if(sound.equals("chess.wav"))
+                    {
+                         System.out.println("son en boucle");
+                         this.s.readAudioFile(path + "lo23/ui/resources/" + sound);
+                         
+                    }
+                    else
+                    {
+                        System.out.println("son court");
+                        n.readAudioFile(path + "lo23/ui/resources/" + sound);
+                    }
 
                 } catch (Exception ex) 
                 {
                     Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
                 }
-             }
+             
         }
         
         public void play()
@@ -53,6 +80,14 @@ public class Launch_Sound implements Runnable{
             enpause = true;
             a.suspend();
         }
+    
+    @Override
+        public void interrupt() {
+        //super.interrupt();
+       
+            n.stop_sound(); // Fermeture du flux si l'interruption n'a pas fonctionné.
+        
+    }
         
                 
         
