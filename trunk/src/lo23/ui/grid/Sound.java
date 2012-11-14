c4fb57622d0fef841a0581f3e03907b696eefd32
guilhem.paoli@gmail.com <guilhem.paoli@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-14 14:50:58 +0000

    update sound

--- trunk/src/lo23/ui/grid/Sound.java
+++ trunk/src/lo23/ui/grid/Sound.java
@@ -1,28 +1,32 @@
 package lo23.ui.grid;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.DataLine.Info;
-import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.AudioSystem;
 import java.io.File;
 import java.io.IOException;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine.Info;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
  
  
 public class Sound
 {
     static SourceDataLine source;
-    static int boucle =1;
+    static boolean boucle = true;
     static boolean lancer = false;
+   
     
-    public static void readAudioFile(String fileName) throws IOException, UnsupportedAudioFileException, LineUnavailableException
+    public  void readAudioFile(String fileName) throws IOException, UnsupportedAudioFileException, LineUnavailableException
     {
-        lancer = true;
-        while(boucle==1)
-        {
-            AudioInputStream ais = AudioSystem.getAudioInputStream(new File(fileName));
+     
+         lancer = true;
+      
+            while(boucle)
+            {
+                AudioInputStream ais = AudioSystem.getAudioInputStream(new File(fileName));
+          
         
             AudioFormat format = ais.getFormat();
             Info info = new Info(SourceDataLine.class, format);
@@ -43,9 +47,9 @@ public class Sound
             source.drain();
             source.close();
             }
-      
       }
     
+     
     public void stop_sound()
     {
         if(lancer == true)
