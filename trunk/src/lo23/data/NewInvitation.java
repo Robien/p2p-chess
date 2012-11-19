4d42e3f93190013f4bf7bf6158a91507bb06a09a
louispontoise@gmail.com <louispontoise@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-19 18:06:49 +0000

    Documentation de NewInvitation.

--- trunk/src/lo23/data/NewInvitation.java
+++ trunk/src/lo23/data/NewInvitation.java
@@ -3,12 +3,22 @@ package lo23.data;
 import java.util.Date;
 import lo23.utils.Enums.COLOR;
 
+/**
+ * Invitation to a new game
+ */
 public class NewInvitation extends Invitation {
 
     private COLOR color;
     private long duration;
     private Date date;
 
+    /**
+     * Constructor for the NewInvitation class
+     * @param color the host's color
+     * @param duration the duration of the game
+     * @param host the host's PublicProfile
+     * @param guest the guest's PublicProfile
+     */
     public NewInvitation(COLOR color, long duration, PublicProfile host, PublicProfile guest) {
         super(host, guest);
         this.color = color;
@@ -16,14 +26,26 @@ public class NewInvitation extends Invitation {
         this.date = new Date();
     }
 
+    /**
+     * Getter for the host's color
+     * @return the color chosen by the host
+     */
     public COLOR getColor() {
         return color;
     }
 
+    /**
+     * Getter for the duration of the game
+     * @return the duration of the game
+     */
     public long getDuration() {
         return duration;
     }
 
+    /**
+     * Getter for the date of the invitation
+     * @return the date of the invitation
+     */
     public Date getDate() {
         return date;
     }
