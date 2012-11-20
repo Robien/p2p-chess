231dae19117573994a3968043a9990928752ed14
noe.gaumont <noe.gaumont@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-20 15:16:08 +0000

    Maj test

--- trunk/src/lo23/data/tests/GameManagerTest.java
+++ trunk/src/lo23/data/tests/GameManagerTest.java
@@ -5,10 +5,16 @@
 package lo23.data.tests;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import lo23.data.ApplicationModel;
 import lo23.data.Game;
+import lo23.data.Move;
 import lo23.data.NewInvitation;
+import lo23.data.Position;
 import lo23.data.Profile;
+import lo23.data.exceptions.FileNotFoundException;
+import lo23.data.exceptions.IllegalMoveException;
 import lo23.data.exceptions.NoIdException;
 import lo23.data.managers.GameManager;
 import lo23.data.managers.ProfileManager;
@@ -26,7 +32,7 @@ public class GameManagerTest {
 
     static private void getListAllGame() throws IOException, NoIdException {
         ApplicationModel app;
-        Profile pHost, pGuest;
+        Profile pGuest;
         NewInvitation inv;
         Game gm;
 
@@ -36,22 +42,46 @@ public class GameManagerTest {
 
         char[] fakePassword = {};
         String profileId = "profileid";
-        Profile p = app.getPManager().createProfile(profileId, "toto", fakePassword, Enums.STATUS.CONNECTED, "", null, "michel", "titi", 22);
-        /*if (app.getPManager().connection(profileId, fakePassword)) {
+        Profile p;
+        try {
+            p = app.getPManager().createProfile(profileId, "toto", fakePassword, Enums.STATUS.CONNECTED, "", null, "michel", "titi", 22);
 
-            pGuest = new Profile("", "host", fakePassword, Enums.STATUS.INGAME, "", null, "", "", 21);
-            inv = new NewInvitation(Enums.COLOR.WHITE, 300, p.getPublicProfile(), pGuest.getPublicProfile());
-            gm = app.getGManager().createGame(inv);
+            if (app.getPManager().connection(profileId, fakePassword)) {
+                System.out.println("Connection.");
+                pGuest = new Profile("", "host", fakePassword, Enums.STATUS.INGAME, "", null, "", "", 21);
+                inv = new NewInvitation(Enums.COLOR.WHITE, 300, app.getPManager().getCurrentProfile().getPublicProfile(), pGuest.getPublicProfile());
 
-            //
-            try {
-                app.getGManager().save();
-            } catch (NoIdException expt) {
-                System.out.println(expt.getMessage());
-                System.out.println(expt.getStackTrace());
+                gm = app.getGManager().createGame(inv);
+                long gid = gm.getGameId(); 
+                
+                //
+                try {
+                    Move m = new Move(new Position(0, 0), new Position(0, 2), gm.getPieceAtXY(0, 0));
+                    try {
+                        gm.playMove(m);
+                    } catch (IllegalMoveException ex) {
+                        Logger.getLogger(TestInit.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    app.getGManager().save();
+                    System.out.println(gid);
+                    app.getGManager().getListAllGames();
+                    app.getGManager().load(gid);
+                    
+                } catch (NoIdException expt) {
+                    System.out.println(expt.getMessage());
+                    System.out.println(expt.getStackTrace());
+                }
+            } else {
+                System.out.println("Probleme lors de la connection.");
             }
-        } else {
-            System.out.println("Probleme lors de la connection.");
-        }*/
+        } catch (IOException ex) {
+            Logger.getLogger(GameManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoIdException ex) {
+            Logger.getLogger(GameManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(GameManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(GameManagerTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 };
