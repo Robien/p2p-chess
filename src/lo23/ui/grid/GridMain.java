261284703d055a397324c0fac9bb7790e974be7e
r.guyard <r.guyard@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-20 13:00:36 +0000

    ajout des fonctions de data pour créer une fake partie

--- src/lo23/ui/grid/GridMain.java
+++ src/lo23/ui/grid/GridMain.java
@@ -4,10 +4,19 @@
  */
 package lo23.ui.grid;
 
+
 //import ui.grid.TMP_GameManager;
 import javax.swing.SwingUtilities;
 import lo23.communication.ComManager;
 import lo23.data.ApplicationModel;
+import lo23.data.Game;
+import lo23.data.NewInvitation;
+import lo23.data.Player;
+import lo23.data.Profile;
+import lo23.data.managers.GameManager;
+import lo23.data.managers.ProfileManager;
+import lo23.utils.Enums.COLOR;
+import lo23.utils.Enums.STATUS;
 
 /**
  *
@@ -37,6 +46,24 @@ public class GridMain {
                  * You can find an example in ChatPanel2 or GamePanel
                  *
                  */
+
+
+                //du code tout moche de data pour faire les tests !
+                        ApplicationModel app = new ApplicationModel();
+
+        app.setGameManager(new GameManager(app));
+        app.setProfileManager(new ProfileManager(app));
+        Player p1 = new Player(COLOR.WHITE, 0, null);
+        Player p2 = new Player(COLOR.BLACK, 0, null);
+
+        char[] fakePassword = {};
+
+        Profile pHost = new Profile("", "host", fakePassword, STATUS.INGAME, "", null, "", "", 21);
+        Profile pGuest = new Profile("", "host", fakePassword, STATUS.INGAME, "", null, "", "", 21);
+        NewInvitation inv = new NewInvitation(COLOR.BLACK, 0, pHost.getPublicProfile(), pGuest.getPublicProfile());
+        Game gm = app.getGManager().createGame(inv);
+
+
                 myModel = new ApplicationModel();
    //             myModel.setGameManager(new TMP_GameManager(myModel));
              //  myModel.setProfileManager(new TMP_ProfileManager(myModel));
