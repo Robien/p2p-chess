9c7a3738cd7a78b7734c8dbaca25d47de39b6b25
karim.chebbour@gmail.com <karim.chebbour@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-11-26 16:47:55 +0000

    Clean code.

--- src/lo23/ui/grid/EventListener.java
+++ src/lo23/ui/grid/EventListener.java
@@ -6,23 +6,30 @@ package lo23.ui.grid;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import lo23.data.ApplicationModel;
 import lo23.data.Move;
+import lo23.data.managers.Manager;
 
 /**
  *
  * @author Karim
  */
 public class EventListener implements PropertyChangeListener {
+    public static final String NEW_EVENT_ADDED = "new-move-added";
+    private ApplicationModel myModel;
+    
     private GamePanel gamePanel;
     
-    public EventListener(GamePanel panel){
+    public EventListener(GamePanel panel, ApplicationModel model){
         gamePanel = panel;
+        myModel = model;
+//        ((Manager)myModel.getGManager()).subscribe(this,NEW_EVENT_ADDED);
     }
     
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        if ("move".equals(evt.getPropertyName())) {
-            gamePanel.updateBoard((Move)evt.getNewValue());
+        if (NEW_EVENT_ADDED.equals(evt.getPropertyName())) {
+          
         }
     }
 }
