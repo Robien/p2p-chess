3e6927078af57ed27c8e86c51d267624e5d2032b
pierre.alexandre.fonta <pierre.alexandre.fonta@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-12-02 15:15:48 +0000

    Serializer::getProfileIds : != instead of equals.

--- src/lo23/data/serializer/Constants.java
+++ src/lo23/data/serializer/Constants.java
@@ -3,14 +3,13 @@ package lo23.data.serializer;
 /**
  * This class contains every path where objects will be serialized and saved
  */
-public class Constants
-{
-	// Paths where will be saved serialized objects
-        // Working directory should be in the netbeans workspace
-           
-	static public final String PROFILES_PATH = System.getProperty("user.dir") + "/data/profiles/";
-	static public final String GAMES_PATH = "data/games/";
-        // File suffixes
-        static public final String PROFILE_SUFFIXE = ".profile";
-        static public final String GAME_SUFFIXE = ".game";
+public class Constants {
+    // Paths where will be saved serialized objects
+    // Working directory should be in the netbeans workspace
+
+    static public final String PROFILES_PATH = System.getProperty("user.dir") + "/data/profiles/";
+    static public final String GAMES_PATH = "data/games/";
+    // File suffixes
+    static public final String PROFILE_SUFFIXE = ".profile";
+    static public final String GAME_SUFFIXE = ".game";
 }
