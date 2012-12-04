5ddef69ccbe3dec16678b2af4ea8e285898b4a3e
stephan.donin@gmail.com <stephan.donin@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2012-12-04 13:28:13 +0000

    lo23.data.exceptions package commented

--- trunk/src/lo23/data/exceptions/WrongInvitation.java
+++ trunk/src/lo23/data/exceptions/WrongInvitation.java
@@ -2,16 +2,23 @@
 package lo23.data.exceptions;
 
 /**
- * Thrown in createGame when a invitation don't contain the currentPrfile
+ * Thrown in createGame when an invitation doesn't contain the currentProfile
  * @author ksadorf
  */
 
 public class WrongInvitation extends Exception {
+    /**
+     * Default constructor
+     */
     public WrongInvitation()
     {
         super();
     }
 
+    /**
+     * Constructor, with a given message
+     * @param msg The message contained into the thrown exception
+     */
     public WrongInvitation(String msg)
     {
         super(msg);
