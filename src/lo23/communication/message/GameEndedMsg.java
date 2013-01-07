429cc24349f806f3abc2fa9266471c32cabef393
berteloot.luc@gmail.com <berteloot.luc@gmail.com@4fab6c43-b6b9-b5f5-0b67-024b0e231051>
2013-01-07 18:14:18 +0000

    Communication : correction for game ended

--- src/lo23/communication/message/GameEndedMsg.java
+++ src/lo23/communication/message/GameEndedMsg.java
@@ -3,7 +3,7 @@ package lo23.communication.message;
 /**
  * This message is sent to notify the distant client that the game is ended.
  */
-public class GameEndedMsg extends ConnectionMessage{
+public class GameEndedMsg extends ConnectionMessage {
 
     /**
      * Constructor of the GameEnded.
